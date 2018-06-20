d00cfd59711f8a67a658a3ec003ec14d9389d7ea
Romeo Folie <romeofolie1@gmail.com>
2018-06-20 22:37:00 +0000

    updated urls

--- calculateInsurance/urls.py
+++ calculateInsurance/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from calculateInsurance import views
 
+app_name = 'calculateInsurance'
 urlpatterns= [
     url(r'^cars/$', views.CarList.as_view(), name='car-list'),
     # url(r'^cars/$', views.car_list, name='car-list'),
--- insurance_calculator_api/urls.py
+++ insurance_calculator_api/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from insurance_calculator_api import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.api_root),
